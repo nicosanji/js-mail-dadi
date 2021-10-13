fa87c9cdf827594140a7a051da0a7a673ebe4864
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-13 15:48:28 +0200

    START: push di controllo

--- js-mail/mail.js
+++ js-mail/mail.js
@@ -1,13 +1,19 @@
 // MAIL.JS 
 
-// 
+// dichiaro le const presenti nel sistema
 
 const mail1 = "p.maldini@gmail.com";
 const mail2 = "f.totti@gmail.com";
 const mail3 = "a.delpiero@gmail.com";
 
-//                   0      1     2    
+//ARRAY(archivio) delle mail presenti nel sistema
+//       indice  =   0      1     2    
 const listaMail = [mail1, mail2, mail3];
 
 console.log(listaMail); 
 
+// dichiaro la const mailUtente facendola inserire all'utente
+
+let mailUtente = prompt("Inserisci la tua e-mail");
+
+// 
