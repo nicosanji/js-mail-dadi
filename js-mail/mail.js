53cc3313be27787b8bd26e4caca08e345881a262
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-13 15:08:34 +0200

    START: js-mail

---
+++ js-mail/mail.js
@@ -0,0 +1,13 @@
+// MAIL.JS 
+
+// 
+
+const mail1 = "p.maldini@gmail.com";
+const mail2 = "f.totti@gmail.com";
+const mail3 = "a.delpiero@gmail.com";
+
+//                   0      1     2    
+const listaMail = [mail1, mail2, mail3];
+
+console.log(listaMail); 
+
